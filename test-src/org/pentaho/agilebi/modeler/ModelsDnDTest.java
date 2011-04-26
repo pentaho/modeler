b828cae0813276c14de81007eb0873d6b2f454c1
Nick Baker <codeoncoffee@gmail.com>
2011-04-26 10:50:56 -0400

    [BISERVER-5923] New tests for drop operations

--- test-src/org/pentaho/agilebi/modeler/ModelsDnDTest.java
+++ test-src/org/pentaho/agilebi/modeler/ModelsDnDTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 public class ModelsDnDTest extends AbstractModelerTest {
 
   @Test
-  public void testValidDrops() throws ModelerException {
+  public void testValidDropIndications() throws ModelerException {
     this.generateTestDomain();
     ModelerWorkspaceHelper helper = new ModelerWorkspaceHelper("en_US");
     helper.autoModelFlat(workspace);
@@ -50,16 +50,13 @@ public class ModelsDnDTest extends AbstractModelerTest {
     // Measure to Hierarchy
     assertTrue(firstHier.acceptsDrop(firstMeasure));
 
-    // Level to measures
-    assertTrue(measures.acceptsDrop(firstLevel));
-
     // measures can be reordered in the measures collection
     assertTrue(measures.acceptsDrop(firstMeasure));
   }
 
 
   @Test
-  public void testInvalidDrops() throws ModelerException {
+  public void testInvalidDropIndications() throws ModelerException {
     this.generateTestDomain();
     ModelerWorkspaceHelper helper = new ModelerWorkspaceHelper("en_US");
     helper.autoModelFlat(workspace);
@@ -113,6 +110,173 @@ public class ModelsDnDTest extends AbstractModelerTest {
     assertFalse(firstDim.acceptsDrop(secondDim));
     assertFalse(firstMeasure.acceptsDrop(secondMeasure));
 
+  }
+
+
+
+  @Test
+  public void testValidDrops() throws ModelerException {
+    this.generateTestDomain();
+    ModelerWorkspaceHelper helper = new ModelerWorkspaceHelper("en_US");
+    helper.autoModelFlat(workspace);
+
+    MeasuresCollection measures = workspace.getModel().getMeasures();
+    AvailableItemCollection items = workspace.getAvailableTables();
+
+    AvailableField firstField = items.getAsAvailableTablesList().get(0).getChildren().get(0);
+    DimensionMetaData firstDim = workspace.getModel().getDimensions().get(0);
+    HierarchyMetaData firstHier = firstDim.get(0);
+    LevelMetaData firstLevel = firstHier.get(0);
+    MeasureMetaData firstMeasure = measures.get(0);
+
+    // can move a field into the measures collection
+    assertNotNull(measures.onDrop(firstField));
+
+    // can move a field into the Dimensions collection
+    assertNotNull(workspace.getModel().getDimensions().onDrop(firstField));
+
+    // available field to dimension
+    assertNotNull(firstDim.onDrop(firstField));
+
+    // measure to dimension
+    assertNotNull(firstDim.onDrop(firstMeasure));
+
+    // Hierarchy to dimension
+    assertNotNull(firstDim.onDrop(firstHier));
+
+    // Level to dimension
+    assertNotNull(firstDim.onDrop(firstLevel));
+
+    // Level to Hierarchy
+    assertNotNull(firstHier.onDrop(firstLevel));
+
+    // Measure to Hierarchy
+    assertNotNull(firstHier.onDrop(firstMeasure));
+
+    // measures can be reordered in the measures collection
+    assertNotNull(measures.onDrop(firstMeasure));
+  }
+
+
+
+  @Test
+  public void testInvalidDrops() throws ModelerException {
+    this.generateTestDomain();
+    ModelerWorkspaceHelper helper = new ModelerWorkspaceHelper("en_US");
+    helper.autoModelFlat(workspace);
+
+    MeasuresCollection measures = workspace.getModel().getMeasures();
+    AvailableItemCollection items = workspace.getAvailableTables();
+    DimensionMetaDataCollection dimensions = workspace.getModel().getDimensions();
+    MainModelNode mainNode = workspace.getModel();
+
+    AvailableField firstField = items.getAsAvailableTablesList().get(0).getChildren().get(0);
+    AvailableField secondField = items.getAsAvailableTablesList().get(0).getChildren().get(1);
+    DimensionMetaData firstDim = workspace.getModel().getDimensions().get(0);
+    DimensionMetaData secondDim = workspace.getModel().getDimensions().get(1);
+    HierarchyMetaData firstHier = firstDim.get(0);
+    HierarchyMetaData secondHier = secondDim.get(0);
+
+    LevelMetaData firstLevel = firstHier.get(0);
+    LevelMetaData secondLevel = secondHier.get(0);
+    MeasureMetaData firstMeasure = measures.get(0);
+    MeasureMetaData secondMeasure = measures.get(1);
+
+
+    //top-down drops are invalid, only bottom up
+    try{
+      assertNull(firstHier.onDrop(firstDim));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstLevel.onDrop(firstDim));
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstLevel.onDrop(firstHier));
+    } catch(ModelerException e){}
+    try{
+      assertNull(dimensions.onDrop(workspace.getModel()));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+
+    //cannot drag dimensions and measures collections not the mainModelNode anywhere
+    try{
+      assertNull(dimensions.onDrop(measures));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(mainNode.onDrop(measures));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstDim.onDrop(measures));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstHier.onDrop(measures));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstLevel.onDrop(measures));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+
+    try{
+      assertNull(measures.onDrop(dimensions));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(mainNode.onDrop(dimensions));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstDim.onDrop(dimensions));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstHier.onDrop(dimensions));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstLevel.onDrop(dimensions));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+
+    try{
+      assertNull(measures.onDrop(mainNode));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(dimensions.onDrop(mainNode));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstDim.onDrop(mainNode));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstHier.onDrop(mainNode));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstLevel.onDrop(mainNode));
+      fail("Should have thrown an exception");
+    } catch(ModelerException e){}
+
+    // same type of node onto another... universally bad
+    try{
+      assertNull(firstLevel.onDrop(secondLevel));
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstHier.onDrop(secondHier));
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstDim.onDrop(secondDim));
+    } catch(ModelerException e){}
+    try{
+      assertNull(firstMeasure.onDrop(secondMeasure));
+    } catch(ModelerException e){}
 
   }
+
 }
